b3704f7887001aae8e263ee2e20afb0e05c0636c
moraru210 <alexandru210moraru@gmail.com>
2022-11-19 10:33:11 +0000

    add a mapping of processing times

--- scheduling.py
+++ scheduling.py
@@ -1,5 +1,7 @@
 def schedule():
-   print("hello world!") 
+   print("hello world!")
+    
 
 if __name__ == "__main__":
-    schedule()
+   processing_times = {"vii": 20.0788, "blur": 5.9768, "night": 23.5906, "onnx": 3.3892, "emboss": 2.2084, "muse": 15.7553, "wave": 11.7784}
+   schedule()
