1a16ff5922633d79e243e5df81b30b428fc615c7
moraru210 <alexandru210moraru@gmail.com>
2022-11-19 10:49:53 +0000

    take in inputs from input.json

--- scheduling.py
+++ scheduling.py
@@ -1,7 +1,20 @@
+import json
+import collections
+
 def schedule():
    print("hello world!")
-    
+
 
 if __name__ == "__main__":
    processing_times = {"vii": 20.0788, "blur": 5.9768, "night": 23.5906, "onnx": 3.3892, "emboss": 2.2084, "muse": 15.7553, "wave": 11.7784}
+   
+   f = open('input.json')
+   input_data = json.load(f)
+
+   adj_list = collections.defaultdict(list)
+   for edge in input_data["workflow_0"]["edge_set"]:
+      adj_list[edge[0]].append(edge[1])
+
+   due_dates = input_data["workflow_0"]["due_dates"]
+   
    schedule()
