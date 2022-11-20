f6f24e45c3e6992ba336acece1f2e70d91edf02f
moraru210 <alexandru210moraru@gmail.com>
2022-11-20 17:54:14 +0000

    Change the order in which we retrieve tasks from get_new_tasks method

--- scheduling.py
+++ scheduling.py
@@ -1,9 +1,10 @@
 import json
 import collections
 import heapq
+import time
 
 class ProcessingFunctions():
-   processing_times = {"vii": 20.0788, "blur": 5.9768, "night": 23.5906, "onnx": 3.3892, "emboss": 2.2084, "muse": 15.7553, "wave": 11.7784}
+   processing_times = {"start": 0, "vii": 20.0788, "blur": 5.9768, "night": 23.5906, "onnx": 3.3892, "emboss": 2.2084, "muse": 15.7553, "wave": 11.7784}
    
    def get_function_time(self, task):
       return self.processing_times[task]
@@ -18,7 +19,7 @@ class Workflow():
    EDGE_SET = 'edge_set'
    START = 'start'
 
-   def __init__(self, input_file='input.json', start='emboss_8', workflow='workflow_0') -> None:
+   def __init__(self, input_file='input.json', start='muse_3', workflow='workflow_0') -> None:
       with open(input_file) as f:
          input_data = json.load(f)
 
@@ -33,7 +34,7 @@ class Workflow():
          an optimisation 
          '''
 
-         causation_map[self.START] = [start]
+         dependency_map[self.START] = [start]
          node_set = set()
          for edge in input_data[workflow][self.EDGE_SET]:
             causation_map[edge[0]].append(edge[1])
@@ -54,22 +55,10 @@ class Workflow():
    was just completed
    '''
    def get_new_tasks(self, completed_tasks):
-      # The task we just completed is last
-      recently_completed_task = completed_tasks[-1]
-      affected_tasks = self.get_causation_map()[recently_completed_task]
-      new_tasks = []
-      for task in affected_tasks:
-         dependencies = self.get_dependency_map()[task]
-         dependencies_met = True
-         for dependency in dependencies:
-            if dependency not in completed_tasks:
-               print('Dependency:', dependency, 'not met for task:', task)
-               dependencies_met = False
-               break
-         if dependencies_met:
-            new_tasks.append(task)
-      print('Task', recently_completed_task, 'enabled', new_tasks)
-      return new_tasks
+      recently_completed_task = completed_tasks[0]
+      dependencies = self.get_dependency_map()[recently_completed_task]
+      # print('Task', recently_completed_task, 'enabled', dependencies)
+      return dependencies
    
    def get_causation_map(self):
       return self.causation_map
@@ -85,9 +74,10 @@ class Workflow():
 
 
 class Schedule():
-   def __init__(self, workflow: Workflow, max_iterations: int=100000) -> None:
+   def __init__(self, workflow: Workflow, max_iterations: int=30000) -> None:
       self.workflow = workflow
       self.max_iterations=max_iterations
+      self.total_num_tasks=32
 
    def sum_task_time(self):
       return ProcessingFunctions().sum_task_time(self.workflow.get_node_set())
@@ -97,73 +87,50 @@ class Schedule():
 
       print(f"total sum: {total_sum}")
       min_heap = [(0, total_sum, ["start"], [])]
-      prev_solution = (0, ["start"])
+      lowest_solution = (float("inf"), ["start"])
       iterations = 0
       while min_heap and iterations <= self.max_iterations:
          lower_bound, sum_so_far, functions_called, possible_paths = heapq.heappop(min_heap)
-         prev_solution = (lower_bound, functions_called)
+         lowest_solution = (lower_bound, functions_called)
+         
+         print(f"Iteration: {iterations}")
+         print(f"\tCurrent Lower Bound Score: {lower_bound}")
+         print(f"\tEnd Time of Current Iteration Node {functions_called[0]} is {sum_so_far}")
+         print(f"\tCurrent Best End Schedule: {functions_called}")
 
          possible_paths = possible_paths + self.workflow.get_new_tasks(functions_called)
          for path in possible_paths:
-            new_called = functions_called + [path]
+            new_called = [path] + functions_called
             new_lower_bound = lower_bound + max(0, sum_so_far - self.workflow.get_due_dates()[path])
             new_paths = possible_paths.copy()
             new_paths.remove(path)
-            heapq.heappush(min_heap, (new_lower_bound, sum_so_far - ProcessingFunctions().get_task_time(path), new_called, new_paths))
+            heapq.heappush(min_heap, (new_lower_bound, sum_so_far - ProcessingFunctions().get_task_time(functions_called[0]), new_called, new_paths))
          iterations += 1
 
       #either we found full solution, or we have to fill in the rest
       #we can fill in the rest according to remainder functions due dates
       print(f"iterations {iterations}")
       if min_heap:
-         return heapq.heappop(min_heap)
+         lower_bound, sum_so_far, functions_called, possible_paths = heapq.heappop(min_heap)
+         print(f"most lower bound solution found {functions_called} with lower bound score {lower_bound}")
+         return self.complete(functions_called, possible_paths)
       else:
-         return prev_solution
+         return lowest_solution
+
+   def complete(self, functions, possible):
+      print(f"Before Completing {functions}")
+      while len(functions) != self.total_num_tasks:
+         possible = possible + self.workflow.get_new_tasks(functions)
+         get_min_due = min(possible, key=lambda x: self.workflow.due_dates[x])
+         functions.insert(0, get_min_due)
+         possible.remove(get_min_due)
+      print(f"Afrer Completing: {functions}")
+      return functions
+
 
 if __name__ == "__main__":
    workflow = Workflow()
+   start_time = time.perf_counter()
    optimal_schedule = Schedule(workflow).schedule()
-   print(optimal_schedule)
-
-
-
-
-
-# def load_graph(N):
-#    G = [[0 for _ in range(1)] for _ in range(N)]
-#    G[0,30]=1
-#    G[1,0]=1
-#    G[2,7]=1
-#    G[3,2]=1
-#    G[4,1]=1
-#    G[5,15]=1
-#    G[6,5]=1
-#    G[7,6]=1
-#    G[8,7]=1
-#    G[9,8]=1
-#    G[10,4]=1
-#    G[11,4]=1
-#    G[12,11]=1
-#    G[13,12]=1
-#    G[14,10]=1
-#    G[15,14]=1
-#    G[16,15]=1
-#    G[17,16]=1
-#    G[18,17]=1
-   # G[19,18]=1
-   # G[20,17]=1
-   # G[21,20]=1
-   # G[22,21]=1
-   # G[23,4]=1
-   # G[24,23]=1
-   # G[25,24]=1
-   # G[26,25]=1
-   # G[27,25]=1
-   # G[28,27]=1
-   # G[29,3]=1
-   # G[29,9]=1
-   # G[29,13]=1
-   # G[29,19]=1
-   # G[29,22]=1
-   # G[29,26]=1
-   # G[29,28]=1
+   print(f"Scheduling took {time.perf_counter() - start_time} seconds")
+   print(f"Schedule found: {optimal_schedule}")
